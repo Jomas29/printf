a8d20b8bbe6387c632fcdf7bfbf69565d5811773
Lukacham <danboyiluka2@gmail.com>
2023-07-25 17:22:09 -0700

    no going back

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _printf - produces output according to a format.
+ * @format: The format string, contains zero or more directives.
+ *
+ * Return: The number of characters printed (excluding the null byte used to end output to strings).
+ */
+int _printf(const char *format, ...);
+int print_string(char *str);
+#endif/* MAIN_H */
